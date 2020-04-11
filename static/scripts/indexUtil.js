e711ee8132a9135531f9169fc75fb5be10bff3fd
Nic Mays <nicmays334@gmail.com>
2020-04-11 15:07:52 -0400

    fixed page timeout with a refresh script.

---
+++ static/scripts/indexUtil.js
@@ -0,0 +1,25 @@
+function init() {
+    console.log('I EXIST');
+    console.log('cunt');
+    bindReloadTimer();
+}
+git
+function bindReloadTimer() {
+    var minutes = 0;
+    var seconds = 0;
+    setInterval(function() {
+        seconds++;
+
+        if(seconds % 60 == 0) {
+            minutes++;
+            console.log(minutes);
+        }
+
+        if(minutes == 45) {
+            window.location.reload();
+        }
+
+    }, 1000);
+}
+
+window.onload = init();
